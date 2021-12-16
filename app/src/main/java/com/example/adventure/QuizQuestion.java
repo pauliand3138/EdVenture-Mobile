114db98deb314f1dd289e4a0e7127a99b65cf282
Paul <paul.iand3138@hotmail.com>
2021-12-16 23:18:18 +0800

    New commit

--- app/src/main/java/com/example/adventure/QuizQuestion.java
+++ app/src/main/java/com/example/adventure/QuizQuestion.java
@@ -3,20 +3,32 @@ package com.example.adventure;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.button.MaterialButton;
 
 public class QuizQuestion extends AppCompatActivity {
     MaterialButton submitButton;
-
+    ImageView backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_question);
         getSupportActionBar().hide();
+
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+
+        });
 
         submitButton = findViewById(R.id.submitButton);
 
