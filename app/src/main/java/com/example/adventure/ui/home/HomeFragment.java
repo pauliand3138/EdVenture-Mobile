8fc77699cd5f51362a1c7bdab92c7137d69915ac
Paul <paul.iand3138@hotmail.com>
2021-12-16 23:46:05 +0800

    Leaderboard

--- app/src/main/java/com/example/adventure/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/adventure/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.adventure.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,10 +14,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.adventure.Home;
+import com.example.adventure.Leaderboard;
 import com.example.adventure.R;
 import com.example.adventure.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+    ImageView leaderboard;
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
@@ -28,7 +33,14 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
+        leaderboard = root.findViewById(R.id.leaderboardImage);
+        leaderboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Leaderboard.class);
+                startActivity(intent);
+            }
+        });
 
         return root;
     }
